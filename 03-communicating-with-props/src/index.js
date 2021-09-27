93f4f03c0d9ba6244506e751708a91290804fd1e
Salimzibarov <rustamse@gmail.com>
2021-09-27 20:10:12 +0500

    Added new react component with injected props (CommentDetail.js)

--- 03-communicating-with-props/src/index.js
+++ 03-communicating-with-props/src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+
 const App = () => {
     return (
-        <div>
-            Hello world
+        <div className="ui container comments">
+            <CommentDetail author="Sam" content="Cool post!" postedTime="Today 6:00 PM"/>
+            <CommentDetail author="Andrey" content="Awful post!" postedTime="Today 5:00 PM"/>
+            <CommentDetail author="Igor" content="It's perfect post!!!" postedTime="Yesterday 6:00 PM"/>
         </div>
     );
 }
 
-ReactDOM.render(<App /> , document.querySelector("#root"));
+ReactDOM.render(<App/>, document.querySelector("#root"));
